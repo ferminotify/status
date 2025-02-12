f1d72988c3ba66e20b3a2e2b3e2276846060bad7
Kevin <kevliujudi@gmail.com>
2025-02-12 23:13:31 +0100

    uh

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/notifier/get/status', async (req, res) => {
     res.header("Access-Control-Allow-Origin", "https://fn.lkev.in");
     res.header("Access-Control-Allow-Origin", "https://ferminotify.lkev.in");
     res.header("Access-Control-Allow-Origin", "https://ferminotify.sirico.dev");
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://localhost:10000");
     res.header("Access-Control-Allow-Methods", "GET");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     if (now - lastLogDate > 600000) {
